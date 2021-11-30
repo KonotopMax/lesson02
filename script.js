9f45884de3452c01c01b3b752a995da534adbfce
Konotop Maxim <konotop.m.i@gmail.com>
2021-11-30 21:07:32 +0300

    Добавил ветку lesson03 с простым заданием

--- script.js
+++ script.js
@@ -10,14 +10,39 @@ console.log(typeof fullPrice);
 console.log(typeof adaptive);
 console.log(screens.length);
 
-let priceOfScreens = screenPrice + "$";
-console.log(priceOfScreens);
+let priceOfScreens;
+console.log(priceOfScreens + "$");
 
-let allFullPrice = fullPrice + "$";
-console.log(allFullPrice);
+let allFullPrice;
+console.log(allFullPrice + "$");
 
 let str = screens.toLowerCase();
 console.log(str.split(", "));
 
 console.log(fullPrice * (rollback / 100));
+
+title = prompt("Как называется ваш проект?");
+screens = prompt("Какие типы экранов нужно разработать? \nпример: Простые, Сложные, Интерактивные");
+screenPrice = parseInt(prompt("Сколько будет стоить данная работа? \nпример: 12000"));
+adaptive = confirm("Нужен ли адаптив на сайте?");
+
+let service1 = prompt("Какой дополнительный тип услуги нужен? \nнапример service1, service2");
+let servicePrice1 = parseInt(prompt("Сколько это будет стоить? \nнапример servicePrice1, servicePrice2"));
+let service2 = prompt("Какой дополнительный тип услуги нужен? \nнапример service1, service2");
+let servicePrice2 = parseInt(prompt("Сколько это будет стоить? \nнапример servicePrice1, servicePrice2"));
+
+fullPrice = screenPrice + servicePrice1 + servicePrice2;
+l
+servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback / 100)));
+console.log(servicePercentPrice);
+
+if (fullPrice > 0 && fullPrice < 15000) {
+  console.log("Скидка не предусмотрена");
+} else if (fullPrice >= 15000 && fullPrice < 30000) {
+  console.log("Даем скидку в 5%");
+} else if (fullPrice >= 30000) {
+  console.log("Даем скидку в 10%");
+} else {
+  onsole.log("Скидка не предусмотрена");
+}
 
